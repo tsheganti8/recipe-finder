6548337ff4b117c3288cc944263975ea924f1900
tsheganti8 <60028926+tsheganti8@users.noreply.github.com>
2025-01-23 18:40:25 -0600

    Recipe Finder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import RecipeFinder from "./components/recipeFinder";
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeFinder/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/recipeFinder.jsx
@@ -0,0 +1,55 @@
+import React,{ useState }  from 'react'
+import useSWR from 'swr';
+import axios from 'axios';
+
+// Axios fetcher function
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+function RecipeFinder() {
+  const[clickedCategory, setClickedCategory] = useState(null);
+  const[clickedImage, setClickedImage] = useState(null);
+  const[clickedDescription, setClickedDescription] = useState(null);
+
+
+  const { data, error, isLoading } = useSWR(
+    'https://www.themealdb.com/api/json/v1/1/categories.php', fetcher
+  );
+
+if(isLoading) return <div>Loading...</div>
+if(error) return <div>Error: {error.message}</div>
+
+  const handleClick = (recipeName, recipeImage, recipeDescription) => {
+    if(recipeImage && recipeDescription){
+      setClickedCategory(recipeName);
+      setClickedImage(recipeImage);
+      setClickedDescription(recipeDescription);
+    }
+  };
+
+
+  
+    return (
+      <div className='main'>
+      <h1 className='recipeHead'>Recipe Finder</h1>
+      <div className="recipes-list">
+      <ul>{data.categories.map((categories) =>(
+        <li key={categories.idCategory} onClick={()=> handleClick(categories.strCategory, categories.strCategoryThumb,categories.strCategoryDescription)}>
+        {categories.strCategory} 
+        </li>
+      ))}
+       {clickedCategory && (
+        <div>
+        <h2>{clickedCategory} </h2>
+        <img 
+        src = {clickedImage}
+        alt={clickedCategory}/>
+        <p>{clickedDescription}</p> 
+        </div>
+       )}
+      </ul>
+      </div>
+      </div>
+    );
+}
+
+export default RecipeFinder;
